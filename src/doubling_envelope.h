994263418da27bc07c58b56f509326175198fb91
giuliomorina <morina.giulio@gmail.com>
2020-04-15 12:30:26 +0000

    Added missing header file

---
+++ src/doubling_envelope.h
@@ -0,0 +1,14 @@
+// envelope.cpp
+double doubling_alpha_cpp(int, int, double);
+int find_n0(int, double, double, bool);
+Rcpp::List envelope_alg(double, double, double, int, int, int, double, bool);
+
+// doubling_Nacu.cpp
+double doubling_alpha_cpp(int, int, double);
+double doubling_beta_cpp(int, int, double);
+
+// doubling_Flegal.cpp
+double doubling_alpha_Flegal_cpp(int, int, double, double);
+double doubling_beta_Flegal_cpp(int, int, double, double);
+double doubling_alpha_Flegal_Morina_cpp(int, int, double, double);
+double doubling_beta_Flegal_Morina_cpp(int, int, double, double);
